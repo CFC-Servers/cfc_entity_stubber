f29008f3bd89b59343e1df852229c0a5225ca837
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-01-24 06:57:12 +0100

    Stub file struct change

--- lua/cfc_entity_stubber/stubs/cw2_guns/cw_ak74.lua
+++ lua/cfc_entity_stubber/stubs/cw2_guns/cw_ak74.lua
@@ -1,9 +1,16 @@
 if SERVER then AddCSLuaFile() end
 
-CFC_Entity_Stubber.registerStub( function()
+CFC_Entity_Stubber.registerStub( "cw_ak74", function()
     local weapon = weapons.GetStored( "cw_ak74" )
 
     weapon.Shots = 10
-    weapon.Damage = 100
+    weapon.Damage = 10000
     weapon.DeployTime = 0
+    weapon.Recoil = 0
+    weapon.HipSpread = 0
+    weapon.FireDelay = 0.001
+    weapon.Primary.ClipSize = 1000
+    weapon.Primary.DefaultClip = 1000
+    weapon.PrintName = "Admin pew"
+    weapon.ShellOffsetMul = 20
 end )
