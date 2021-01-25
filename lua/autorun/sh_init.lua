5c3161fb1f625080940302cbe503e92e926b7467
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-01-25 05:37:54 +0100

    Remove old files

--- lua/autorun/sh_init.lua
+++ lua/autorun/sh_init.lua
@@ -1,7 +1,5 @@
 CFC_Entity_Stubber = {}
 
 AddCSLuaFile( "cfc_entity_stubber/shared/sh_entstubber.lua" )
-AddCSLuaFile( "cfc_entity_stubber/shared/plugins/sh_cw_attachments.lua" )
 
 include( "cfc_entity_stubber/shared/sh_entstubber.lua" )
-include( "cfc_entity_stubber/shared/plugins/sh_cw_attachments.lua" )
