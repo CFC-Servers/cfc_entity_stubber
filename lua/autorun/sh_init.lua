05b2a48a93c0efe50aa68a1c5e628e949d6484aa
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-01-25 02:36:02 +0100

    Add new attachments lua file

--- lua/autorun/sh_init.lua
+++ lua/autorun/sh_init.lua
@@ -1,2 +1,5 @@
 AddCSLuaFile( "cfc_entity_stubber/shared/sh_entstubber.lua" )
-include( "cfc_entity_stubber/shared/sh_entstubber.lua" )
+AddCSLuaFile( "cfc_entity_stubber/shared/sh_entstubber.lua" )
+
+include( "cfc_entity_stubber/shared/plugins/sh_cw_attachments.lua" )
+include( "cfc_entity_stubber/shared/plugins/sh_cw_attachments.lua" )
