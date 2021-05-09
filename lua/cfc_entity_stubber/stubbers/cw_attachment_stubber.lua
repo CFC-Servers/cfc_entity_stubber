7c2ac247fe40f8bcc4ca4342720f64c3fa431209
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-05-10 00:43:11 +0200

    Add attachment support

---
+++ lua/cfc_entity_stubber/stubbers/cw_attachment_stubber.lua
@@ -0,0 +1,18 @@
+AddCSLuaFile( )
+cfcEntityStubber.registerStubber( "cw_attachment" )
+
+function cfcEntityStubber.getAttachment( attachName )
+    local attachment = CustomizableWeaponry.registeredAttachmentsSKey[attachName]
+    cfcEntityStubber.oldWeaponStats[attachName] = attachment
+
+    return attachment
+end
+
+function cfcEntityStubber.applyAttachmentChange( attachment )
+    CustomizableWeaponry:registerAttachment( attachment )
+
+    if CLIENT then
+        attachment["description"] = { }
+        CustomizableWeaponry:createStatText( attachment )
+    end
+end
