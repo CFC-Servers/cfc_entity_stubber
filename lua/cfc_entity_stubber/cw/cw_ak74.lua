2d87d2ab1e7cc0f2e3f70f2dc0ed3ed040079ac9
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-05-10 00:18:59 +0200

    Init

---
+++ lua/cfc_entity_stubber/cw/cw_ak74.lua
@@ -0,0 +1,15 @@
+AddCSLuaFile( )
+
+cfcEntityStubber.registerStub( function( )
+    local weapon = cfcEntityStubber.getWeapon( "cw_ak74" )
+    weapon.Shots = 10
+    weapon.Damage = 10000
+    weapon.DeployTime = 0
+    weapon.Recoil = 0
+    weapon.HipSpread = 0
+    weapon.FireDelay = 0.001
+    weapon.Primary.ClipSize = 1000
+    weapon.Primary.DefaultClip = 1000
+    weapon.PrintName = "Admin pew"
+    weapon.ShellOffsetMul = 20
+end )
