2a7d09f68e5ad591116f423ce129a86d2f1d0235
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-05-23 05:02:48 +0200

    Delete m9k_ak47.lua

--- lua/cfc_entity_stubber/m9k/m9k_assault_rifles/m9k_ak47.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile()
-
-cfcEntityStubber.registerStub( function()
-    local weapon = cfcEntityStubber.getWeapon( "m9k_ak47" )
-    weapon.Primary.KickDown = 0.1
-    weapon.Primary.Damage = 10000
-    weapon.Primary.Spread = .08
-end )
