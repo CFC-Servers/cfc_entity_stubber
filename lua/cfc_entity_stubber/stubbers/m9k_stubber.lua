51a74c264b8232bd75bddf2e5eb19d97c1d0af3f
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2021-05-24 11:11:30 -0700

    Add meme minigun

--- lua/cfc_entity_stubber/stubbers/m9k_stubber.lua
+++ lua/cfc_entity_stubber/stubbers/m9k_stubber.lua
@@ -1,2 +1,22 @@
 AddCSLuaFile()
 cfcEntityStubber.registerStubber( "m9k" )
+
+
+CFC_M9k_Stubber.registerStub( function()
+    local SWEP = weapons.GetStored( "m9k_minigun" )
+    
+    SWEP.Primary.RPM = 1500
+    SWEP.Primary.ClipSize = 300
+    SWEP.Primary.DefaultClip = 0
+
+    SWEP.Primary.KickUp = 1.5
+    SWEP.Primary.KickDown = 0.2
+    SWEP.Primary.KickHorizontal = 0.2
+    SWEP.Primary.Automatic = true
+    SWEP.Primary.Ammo = "ar2"
+    SWEP.Primary.NumShots = 2
+    SWEP.Primary.Damage = 6
+    SWEP.Primary.Spread = .06
+    SWEP.Primary.IronAccuracy = .08
+    
+end )
