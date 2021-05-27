9f685ad3a9a44919ab9eebd7ed1127fd9d0451b3
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2021-05-27 13:07:56 -0700

    update formatting

--- lua/cfc_entity_stubber/m9k/heavy_weapons/m9k_minigun.lua
+++ lua/cfc_entity_stubber/m9k/heavy_weapons/m9k_minigun.lua
@@ -3,17 +3,18 @@ AddCSLuaFile()
 CFC_M9k_Stubber.registerStub( function()
     local SWEP = cfcEntityStubber.getWeapon( "m9k_minigun" )
     
-    SWEP.Primary.RPM = 1500
-    SWEP.Primary.ClipSize = 300
-    SWEP.Primary.DefaultClip = 0
-
-    SWEP.Primary.KickUp = 1.5
-    SWEP.Primary.KickDown = 0.2
-    SWEP.Primary.KickHorizontal = 0.2
-    SWEP.Primary.Automatic = true
-    SWEP.Primary.Ammo = "ar2"
-    SWEP.Primary.NumShots = 2
-    SWEP.Primary.Damage = 6
-    SWEP.Primary.Spread = .06
-    SWEP.Primary.IronAccuracy = .08
+    SWEP.Primary.RPM                = 1500
+    SWEP.Primary.ClipSize           = 300
+    SWEP.Primary.DefaultClip        = 0
+    SWEP.Primary.KickUp             = 1.5
+    SWEP.Primary.KickDown           = 0.2
+    SWEP.Primary.KickHorizontal     = 0.2
+    
+    SWEP.Primary.Automatic          = true
+    SWEP.Primary.Ammo               = "ar2"
+    SWEP.Primary.NumShots           = 2
+    SWEP.Primary.Damage             = 6
+    SWEP.Primary.Spread             = .06
+    SWEP.Primary.IronAccuracy       = .08
 end )
+
