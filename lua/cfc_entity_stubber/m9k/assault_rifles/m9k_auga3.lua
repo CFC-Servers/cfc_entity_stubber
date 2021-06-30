ace83dffc96985998a7f59a637296f24e45a5a04
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-06-30 07:19:59 +0200

    Apply aug chance

--- lua/cfc_entity_stubber/m9k/assault_rifles/m9k_auga3.lua
+++ lua/cfc_entity_stubber/m9k/assault_rifles/m9k_auga3.lua
@@ -5,13 +5,15 @@ cfcEntityStubber.registerStub( function()
     weapon.Primary.RPM = 700
     weapon.Primary.ClipSize = 30
     weapon.Primary.DefaultClip = 60
-    weapon.Primary.KickUp = .4
-    weapon.Primary.KickDown = .2
-    weapon.Primary.KickHorizontal = .5
+    weapon.Primary.KickUp = 1.5
+    weapon.Primary.KickDown = 1.75
+    weapon.Primary.KickHorizontal = 1.5
     weapon.Primary.Automatic = true
     weapon.Primary.Ammo = "ar2"
     weapon.Primary.NumShots = 1
-    weapon.Primary.Damage = 18
-    weapon.Primary.Spread = .05
+    weapon.Primary.Damage = 35
+    weapon.Primary.Spread = .03
+    weapon.Primary.SpreadZoomed = 0.01
     weapon.Primary.IronAccuracy = .02
+    weapon.Primary.SpreadBefore = 0.03
 end )
