1cd50791d1f8b5edd3a3dc3f43546f0bb755f5f6
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-07-20 05:04:39 +0200

    Add stub files

---
+++ lua/cfc_entity_stubber/m9k/m9k_scoped_base.lua
@@ -0,0 +1,47 @@
+AddCSLuaFile()
+
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "bobs_scoped_base" )
+    weapon.ShouldDoMoveSpread = false
+
+    function weapon:IronSight()
+        local owner = self:GetOwner()
+        if owner:GetViewEntity() ~= owner or not self:GetNWBool("CanIronSights") then return end
+
+        if owner:KeyPressed( IN_ATTACK2 ) then
+            owner:SetFOV( 10, 0.2 )
+            self.DrawCrosshair = false
+            self:SetNWInt( "ScopeState", 1 )
+            owner:DrawViewModel( false )
+            owner:EmitSound("weapons/zoom.wav")
+        elseif owner:KeyReleased( IN_ATTACK2 ) then
+            owner:SetFOV( 0, 0.1 )
+            self.DrawCrosshair = true
+            self:SetNWInt( "ScopeState", 0 )
+            owner:DrawViewModel( true )
+            owner:EmitSound("weapons/zoom.wav")
+        end
+    end
+
+    function weapon:Think()
+        self:IronSight()
+
+        if self:GetNWInt( "ScopeState" ) > 0 then
+            self.Primary.Spread = self.Primary.SpreadZoomed
+        else
+            self.Primary.Spread = self.Primary.SpreadBefore
+        end
+    end
+
+    function SWEP:AdjustMouseSensitivity()
+        local Scope = self:GetNWInt( "ScopeState" )
+
+        if Scope == 1 then
+            return 0.1
+        end
+    end
+
+    function weapon:SecondaryAttack()
+        return false
+    end
+end )
