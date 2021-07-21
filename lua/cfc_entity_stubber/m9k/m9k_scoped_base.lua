f9936ed9135892363d2c9532a72312fb6bff951e
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-07-21 06:11:32 +0200

    Fix bolt action scoped weapons

--- lua/cfc_entity_stubber/m9k/m9k_scoped_base.lua
+++ lua/cfc_entity_stubber/m9k/m9k_scoped_base.lua
@@ -11,7 +11,7 @@ cfcEntityStubber.registerStub( function()
         if owner:KeyPressed( IN_ATTACK2 ) then
             owner:SetFOV( 10, 0.2 )
             self.DrawCrosshair = false
-            self:SetNWInt( "ScopeState", 1 )
+            self:SetNWInt( "ScopeState", 10000 )
             owner:DrawViewModel( false )
             owner:EmitSound("weapons/zoom.wav")
         elseif owner:KeyReleased( IN_ATTACK2 ) then
@@ -21,6 +21,13 @@ cfcEntityStubber.registerStub( function()
             owner:DrawViewModel( true )
             owner:EmitSound("weapons/zoom.wav")
         end
+    end
+
+    local stubFunc = weapon:PrimaryAttack()
+
+    function weapon:PrimaryAttack()
+        owner:DrawViewModel( true )
+        stubFunc()
     end
 
     function weapon:Think()
@@ -33,7 +40,7 @@ cfcEntityStubber.registerStub( function()
         end
     end
 
-    function SWEP:AdjustMouseSensitivity()
+    function weapon:AdjustMouseSensitivity()
         local Scope = self:GetNWInt( "ScopeState" )
 
         if Scope == 1 then
@@ -41,21 +48,24 @@ cfcEntityStubber.registerStub( function()
         end
     end
 
-    function SWEP:Reload()
-        if SERVER and game.SinglePlayer() then self:CallOnClient("Reload") end
+    function weapon:Reload()
+        if SERVER and game.SinglePlayer() then self:CallOnClient( "Reload" ) end
+        timer.Remove("m9k_resetscope_" .. self.OurIndex)
+
+        local owner = self:GetOwner()
 
         if self:GetNWInt("ScopeState") > 0 then
             owner:SetFOV( 0, 0.1 )
             self.DrawCrosshair = true
             self:SetNWInt( "ScopeState", 0 )
             owner:DrawViewModel( true )
-            owner:EmitSound("weapons/zoom.wav")
+            owner:EmitSound( "weapons/zoom.wav" )
             self.NextReloadTime = CurTime() + 0.5
         end
 
-        if self.CanReload and self.NextReloadTime < CurTime() and self:GetOwner():GetAmmoCount(self.Primary.Ammo) >= 1 and self:Clip1() < self:GetMaxClip1() then
-            self:DefaultReload(ACT_VM_RELOAD)
-            self:GetOwner():SetAnimation(PLAYER_RELOAD)
+        if self.CanReload and self.NextReloadTime < CurTime() and owner:GetAmmoCount(self.Primary.Ammo) >= 1 and self:Clip1() < self:GetMaxClip1() then
+            self:DefaultReload( ACT_VM_RELOAD )
+            owner:SetAnimation( PLAYER_RELOAD )
         end
     end
 
