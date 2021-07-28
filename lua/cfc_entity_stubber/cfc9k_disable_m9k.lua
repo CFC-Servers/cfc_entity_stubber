b67f93ff20abef65a92664b4ed406700178c2180
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-07-28 02:41:47 -0700

    Refactor base stub

--- lua/cfc_entity_stubber/cfc9k_disable_m9k.lua
+++ lua/cfc_entity_stubber/cfc9k_disable_m9k.lua
@@ -1,12 +1,6 @@
 AddCSLuaFile()
-local startsWith = string.StartWith
 
-for _, wep in ipairs( weapons.GetLis() ) do
-    local className = wep.ClassName
-
-    if startsWith( className, "m9k_" ) then
-        local weaponRef = weapons.GetStored( className )
-        weaponRef.Spawnable = false
-        weaponRef.AdminSpawnable = true
-    end
-end
+local base = cfcEntityStubber.getWeapon( "bobs_gun_base" )
+base.Spawnable = false
+base.AdminSpawnable = true
+base.ShouldDoMoveSpread = false
