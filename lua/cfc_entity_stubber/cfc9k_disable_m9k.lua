881b9ada6c2eb0172508481376ba5343b235196c
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-07-29 15:40:43 -0700

    Refactor base stubber, fix ironaccuracy

--- lua/cfc_entity_stubber/cfc9k_disable_m9k.lua
+++ lua/cfc_entity_stubber/cfc9k_disable_m9k.lua
@@ -1,15 +1,2 @@
 AddCSLuaFile()
 
-local base = cfcEntityStubber.getWeapon( "bobs_gun_base" )
-base.Spawnable = false
-base.AdminSpawnable = true
-base.ShouldDoMoveSpread = false
-
-base.OriginalShootBullet = base.ShootBullet
-base.ShootBullet = function( self, damage, _, numBullets, spread )
-    if self.IronSightState then
-        spread = self.IronAccuracy or spread
-    end
-
-    return self:OriginalShootBullet( damage, _, numBullets, spread )
-end
