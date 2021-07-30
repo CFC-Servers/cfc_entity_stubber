47e7425a332fc976264403aa9f953d3559029f39
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-07-29 18:53:51 -0700

    Add Pistol variants

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_pistol_base.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_pistol_base.lua
@@ -1,19 +1,23 @@
 AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
-    local weapon = cfcEntityStubber.getWeapon( "m9k_m92beretta" )
-    weapon.Category = "CFC9k Pistols"
-    weapon.Purpose = "A basic, all-around pistol"
+    local weapons = { "m9k_m92beretta", "m9k_luger" }
 
-    weapon.Primary.RPM = 500
-    weapon.Primary.ClipSize = 12
-    weapon.Primary.KickUp = 1.25
-    weapon.Primary.KickDown = 0.5
-    weapon.Primary.KickHorizontal = 0.3
-    weapon.Primary.Automatic = false
-    weapon.Primary.Ammo = "pistol"
-    weapon.Primary.NumShots = 1
-    weapon.Primary.Damage = 25
-    weapon.Primary.Spread = 0.03
-    weapon.Primary.IronAccuracy = 0.02
+    for _, weaponClass in ipairs( weapons ) do
+        local weapon = cfcEntityStubber.getWeapon( weaponClass )
+        weapon.Purpose = "A basic, all-around pistol"
+        weapon.CFC_Category = "Pistol:Base"
+
+        weapon.Primary.RPM = 500
+        weapon.Primary.ClipSize = 12
+        weapon.Primary.KickUp = 1.25
+        weapon.Primary.KickDown = 0.5
+        weapon.Primary.KickHorizontal = 0.3
+        weapon.Primary.Automatic = false
+        weapon.Primary.Ammo = "pistol"
+        weapon.Primary.NumShots = 1
+        weapon.Primary.Damage = 25
+        weapon.Primary.Spread = 0.03
+        weapon.Primary.IronAccuracy = 0.02
+    end
 end )
