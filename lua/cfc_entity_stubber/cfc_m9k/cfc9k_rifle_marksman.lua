f8f42be526a225dcc0322da0fd1a1fbda5adff82
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-07-29 20:15:05 -0700

    Add rifle groups

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_rifle_marksman.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_rifle_marksman.lua
@@ -1,19 +1,25 @@
 AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
-    local weapon = cfcEntityStubber.getWeapon( "m9k_m14sp" )
-    weapon.Category = "CFC9k Assault Rifles"
-    weapon.Spawnable = true
+    local weapons = {
+        "m9k_m14sp",
+        "m9k_winchester73"
+    }
 
-    weapon.Primary.RPM = 400
-    weapon.Primary.ClipSize = 15
+    for _, weaponClass in ipairs( weapons ) do
+        local weapon = cfcEntityStubber.getWeapon( weaponClass )
+        weapon.Purpose = ""
+        weapon.CFC_Category = "Rifle:Marksman"
 
-    weapon.Primary.KickUp = 2
-    weapon.Primary.KickDown = 1
-    weapon.Primary.KickHorizontal = 0.3
-    weapon.Primary.Automatic = false
-    weapon.Primary.Ammo = "ar2"
-    weapon.Primary.Damage = 55
-    weapon.Primary.Spread = 0.07
-	weapon.Primary.IronAccuracy = 0.01
+        weapon.Primary.RPM = 400
+        weapon.Primary.ClipSize = 15
+        weapon.Primary.KickUp = 2
+        weapon.Primary.KickDown = 1
+        weapon.Primary.KickHorizontal = 0.3
+        weapon.Primary.Automatic = false
+        weapon.Primary.Ammo = "ar2"
+        weapon.Primary.Damage = 55
+        weapon.Primary.Spread = 0.07
+        weapon.Primary.IronAccuracy = 0.01
+    end
 end )
