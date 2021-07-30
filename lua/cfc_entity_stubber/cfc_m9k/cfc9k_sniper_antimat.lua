9d45487215d9ec1bd279099f40f917803df15b91
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-07-29 20:26:41 -0700

    Add sniper groups

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_sniper_antimat.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_sniper_antimat.lua
@@ -1,17 +1,22 @@
 AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
-    local weapon = cfcEntityStubber.getWeapon( "m9k_barret_m82" )
-    weapon.Category = "CFC9k Sniper Rifles"
-    weapon.Spawnable = true
+    local weapons = {
+        "m9k_barret_m82"
+    }
 
-    weapon.Primary.RPM = 100
-    weapon.Primary.ClipSize = 5
+    for _, weaponClass in ipairs( weapons ) do
+        local weapon = cfcEntityStubber.getWeapon( weaponClass )
+        weapon.Purpose = ""
+        weapon.CFC_Category = "Sniper:AntiMat"
 
-    weapon.Primary.KickUp = 3
-    weapon.Primary.KickDown = 1
-    weapon.Primary.KickHorizontal = 5
-    weapon.Primary.Damage = 5000
-    weapon.Primary.Spread = 0.17
-	weapon.Primary.IronAccuracy = 0.02
+        weapon.Primary.RPM = 100
+        weapon.Primary.ClipSize = 5
+        weapon.Primary.KickUp = 3
+        weapon.Primary.KickDown = 1
+        weapon.Primary.KickHorizontal = 5
+        weapon.Primary.Damage = 5000
+        weapon.Primary.Spread = 0.17
+        weapon.Primary.IronAccuracy = 0.02
+    end
 end )
--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_sniper_semi.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_sniper_semi.lua
@@ -1,17 +1,25 @@
 AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
-    local weapon = cfcEntityStubber.getWeapon( "m9k_dragunov" )
-    weapon.Category = "CFC9k Sniper Rifles"
-    weapon.Spawnable = true
+    local weapons = {
+        "m9k_dragunov",
+        "m9k_psg1",
+        "m9k_svu",
+        "m9k_svt40"
+    }
 
-    weapon.Primary.RPM = 400
-    weapon.Primary.ClipSize = 15
+    for _, weaponClass in ipairs( weapons ) do
+        local weapon = cfcEntityStubber.getWeapon( weaponClass )
+        weapon.Purpose = ""
+        weapon.CFC_Category = "Sniper:Semi"
 
-    weapon.Primary.KickUp = 2
-    weapon.Primary.KickDown = 1
-    weapon.Primary.KickHorizontal = 1
-    weapon.Primary.Damage = 55
-    weapon.Primary.Spread = 0.15
-	weapon.Primary.IronAccuracy = 0.01
+        weapon.Primary.RPM = 400
+        weapon.Primary.ClipSize = 15
+        weapon.Primary.KickUp = 2
+        weapon.Primary.KickDown = 1
+        weapon.Primary.KickHorizontal = 1
+        weapon.Primary.Damage = 55
+        weapon.Primary.Spread = 0.15
+        weapon.Primary.IronAccuracy = 0.01
+    end
 end )
