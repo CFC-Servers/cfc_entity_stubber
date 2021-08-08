54eb388337b913b0a22b9be4b6f872edf70aad92
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2021-08-08 15:22:26 -0600

    cfc9k_hotfix_doublebarrelshotgun_damage
    Correct double barrel shotgun damage model.

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_shotgun_elephant.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_shotgun_elephant.lua
@@ -16,7 +16,7 @@ cfcEntityStubber.registerStub( function()
         weapon.Primary.KickDown = 2
         weapon.Primary.KickHorizontal = 5
         weapon.Primary.NumShots = 12
-        weapon.Primary.Damage = 125
+        weapon.Primary.Damage = 15
         weapon.Primary.Spread = 0.1
     end
 end )
