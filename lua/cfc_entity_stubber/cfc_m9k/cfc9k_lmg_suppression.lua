1cf53795ec797b6d12e057ddd528ad6eba65ae6c
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2021-08-25 10:54:10 -0600

    cfc9k_v1.3.110
    Changes naming convention to better adhere to industry standards. Slightly improves the minigun.

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_lmg_suppression.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_lmg_suppression.lua
@@ -16,9 +16,9 @@ cfcEntityStubber.registerStub( function()
         weapon.Primary.ClipSize = 150
         weapon.Primary.KickUp = 0.9
         weapon.Primary.KickDown = 0
-        weapon.Primary.KickHorizontal = 0.7
+        weapon.Primary.KickHorizontal = 0.4
         weapon.Primary.NumShots = 2
-        weapon.Primary.Damage = 15
+        weapon.Primary.Damage = 17
         weapon.Primary.Spread = 0.038
     end
 end )
