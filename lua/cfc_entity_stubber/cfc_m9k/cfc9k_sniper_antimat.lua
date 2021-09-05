c20aa195b5545c24acfb08ed5e7cfd590a8a9bf0
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2021-09-05 15:47:11 -0600

    cfc9k_v1.3.112
    Adds "weapon.PrintName" to the antimat sniper category.  fills out purpose field for anti mat sniper category.

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_sniper_antimat.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_sniper_antimat.lua
@@ -7,8 +7,9 @@ cfcEntityStubber.registerStub( function()
 
     for _, weaponClass in ipairs( weapons ) do
         local weapon = cfcEntityStubber.getWeapon( weaponClass )
-        weapon.Purpose = ""
+        weapon.Purpose = "Strong against props at medium range, but too inacurate to effectivly hit infantry."
         weapon.CFC_Category = "Sniper:AntiMat"
+		weapon.PrintName = "Anti-Materiel Rifle"
 
         weapon.Primary.RPM = 100
         weapon.Primary.ClipSize = 5
