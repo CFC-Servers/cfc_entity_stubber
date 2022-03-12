c4da4390b42e36624222eefb3054ae78e638ff35
Pierce <pierce@vulpes.dev>
2022-03-12 18:21:17 -0500

    delete all build kill specific files

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_bobs_gun_base.lua
+++
@@ -1,6 +0,0 @@
-AddCSLuaFile()
-
-cfcEntityStubber.registerStub( function()
-    local weapon = cfcEntityStubber.getWeapon( "bobs_gun_base" )
-    weapon.ShouldDoMoveSpread = false
-end )
--- lua/cfc_entity_stubber/stubbers/cw_stubber.lua
+++
@@ -1,2 +0,0 @@
-AddCSLuaFile()
-cfcEntityStubber.registerStubber( "cw" )
